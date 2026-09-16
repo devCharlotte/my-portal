85a8b0faac858c564d18cd415d280faf0e26498c
jo0on <workjh77@gmail.com>
2026-09-16 17:15:26 +0900

    Refresh TA presenter installer v13

--- work/my-apk-extension/ta-windows-galaxy-tab-auto-presenter/TA-Windows-Galaxy-Tab-Auto-Presenter-Install.cmd
+++ work/my-apk-extension/ta-windows-galaxy-tab-auto-presenter/TA-Windows-Galaxy-Tab-Auto-Presenter-Install.cmd
@@ -4,7 +4,7 @@ set "TMPDIR=%TEMP%\TA-Windows-Galaxy-Tab-Auto-Presenter-Installer"
 if not exist "%TMPDIR%" mkdir "%TMPDIR%" >nul 2>&1
 set "SCRIPT=%TMPDIR%\TA-Windows-Galaxy-Tab-Auto-Presenter.ps1"
 
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$ProgressPreference='SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing -Uri 'https://devcharlotte.github.io/my-portal/work/my-apk-extension/ta-windows-galaxy-tab-auto-presenter/TA-Windows-Galaxy-Tab-Auto-Presenter.ps1?v=2026-09-16-12' -OutFile '%SCRIPT%'"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$ProgressPreference='SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing -Uri 'https://devcharlotte.github.io/my-portal/work/my-apk-extension/ta-windows-galaxy-tab-auto-presenter/TA-Windows-Galaxy-Tab-Auto-Presenter.ps1?v=2026-09-16-13' -OutFile '%SCRIPT%'"
 
 if errorlevel 1 (
   echo.
