1e57a3d495725254e66da279196884a3e293fb71
jo0on <workjh77@gmail.com>
2026-09-16 17:12:09 +0900

    Remove obsolete TA-Windows-Galaxy-Tab-Auto-Presenter-Stop.cmd

--- work/my-apk-extension/ta-windows-galaxy-tab-auto-presenter/TA-Windows-Galaxy-Tab-Auto-Presenter-Stop.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "SCRIPT=%TEMP%\TA-Windows-Galaxy-Tab-Auto-Presenter\TA-Windows-Galaxy-Tab-Auto-Presenter.ps1"
-if not exist "%SCRIPT%" exit /b 0
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT%" -Mode stop
