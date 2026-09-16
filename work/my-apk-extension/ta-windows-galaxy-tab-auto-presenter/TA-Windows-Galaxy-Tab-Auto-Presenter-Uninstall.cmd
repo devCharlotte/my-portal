bf99562a678d2697f14c9262909f487e84f4c7b2
jo0on <workjh77@gmail.com>
2026-09-16 17:25:32 +0900

    Remove obsolete presenter launcher

--- work/my-apk-extension/ta-windows-galaxy-tab-auto-presenter/TA-Windows-Galaxy-Tab-Auto-Presenter-Uninstall.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "SCRIPT=%LOCALAPPDATA%\TA-Windows-Galaxy-Tab-Auto-Presenter\TA-Windows-Galaxy-Tab-Auto-Presenter.ps1"
-if not exist "%SCRIPT%" (
-  echo TA Windows Galaxy Tab Auto Presenter is not installed.
-  pause
-  exit /b 1
-)
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT%" -Mode uninstall
